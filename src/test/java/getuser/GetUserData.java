2a21d34e5a5b697d4fe26b699045529cbc955542
aabraham <aabraham@SIGNIFI-308.ssi.main>
2023-08-02 12:37:15 -0400

    First Commit

---
+++ src/test/java/getuser/GetUserData.java
@@ -0,0 +1,10 @@
+package getuser;
+
+
+
+public class GetUserData {
+
+	 public static  String base="https://reqres.in/api/users?page=2";
+}
+	
+
---
+++ src/test/java/getuser/Tests.java
@@ -0,0 +1,21 @@
+/*package getuser;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+import io.restassured.response.Response;
+
+public class Tests {
+	
+	@Test
+	public void testgetuser()
+	
+	{
+		Response res=Endpoints.getuser(2,2);
+		res.then().log().all();
+		Assert.assertEquals(res.getStatusCode(),200);
+	
+	
+
+}
+}*/
